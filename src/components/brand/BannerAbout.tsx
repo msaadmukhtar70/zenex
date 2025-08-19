181c7c9ac3008badf64019bf25ef80700453de92
msaadmukhtar70 <msaadmukhtar70@gmail.com>
2025-08-19 13:20:01 +0500

    devibi logo

--- src/components/brand/BannerAbout.tsx
+++ src/components/brand/BannerAbout.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Image from 'next/image';
-import logo from '@/assets/img/Devibilogowhite.png';
+import logo from '@/assets/img/Devibilogowhite.svg';
 
 const BannerAbout = () => {
   return (
--- src/components/brand/MarqueeAreaHomeOne.tsx
+++ src/components/brand/MarqueeAreaHomeOne.tsx
@@ -9,7 +9,7 @@ const MarqueeAreaHomeOne = () => {
       <div className="cs_moving_section_wrap cs_bold">
         <div className="cs_moving_section_in">
           <div className="cs_moving_section cs_stroke_text">
-            Devibi — Where Creativity Meets Performance
+            Devibi — Where Creativity Meets Performance   
           </div>
           <div className="cs_moving_section cs_stroke_text">
             Powering Ideas. Designing Futures. Growing Brands.
